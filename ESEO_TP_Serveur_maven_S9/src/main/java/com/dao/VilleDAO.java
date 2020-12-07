9b3f75014b9135c0ccb9070723577176d9038622
axelt <axelt@DESKTOP-B8R2PIK>
2020-12-07 23:11:24 +0100

    Modification final

--- ESEO_TP_Serveur_maven_S9/src/main/java/com/dao/VilleDAO.java
+++ ESEO_TP_Serveur_maven_S9/src/main/java/com/dao/VilleDAO.java
@@ -6,6 +6,14 @@ import com.dto.Ville;
 
 public interface VilleDAO {
 	
-	public ArrayList<Ville> findAllVilles();
+	
+	public ArrayList<Ville> getInfoVille();
 
+	public void setVille(Ville ville);
+
+	public ArrayList<Ville> getInfoVilles(String param);
+
+	public void mettreAJour(Ville ville);
+
+	public void supprimerLigne(String code_commune_INSEE);
 }
