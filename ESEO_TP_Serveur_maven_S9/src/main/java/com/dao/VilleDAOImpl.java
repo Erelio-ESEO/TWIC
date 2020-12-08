ef116dbe4e52e8f8452c9634ae915bfb8553d802
axelt <axelt@DESKTOP-B8R2PIK>
2020-12-08 02:33:54 +0100

    Correction bug #6

--- ESEO_TP_Serveur_maven_S9/src/main/java/com/dao/VilleDAOImpl.java
+++ ESEO_TP_Serveur_maven_S9/src/main/java/com/dao/VilleDAOImpl.java
@@ -16,14 +16,14 @@ public class VilleDAOImpl implements VilleDAO {
 	public ArrayList<Ville> getInfoVille() {
 		Ville ville = null;
 		ArrayList<Ville> villes = new ArrayList<Ville>();
-		
+		Connection con = JDBCConfiguration.getonnexionBDD();
 
 		String requete = "SELECT * FROM ville_france";
-
+		Statement stmt = null;
+		ResultSet rs = null;
 		try {
-			Connection con = JDBCConfiguration.getonnexionBDD();
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(requete);
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(requete);
 			while (rs.next()) {
 				ville = new Ville();
 				ville.setCodeCommune(rs.getString(1));
@@ -36,10 +36,27 @@ public class VilleDAOImpl implements VilleDAO {
 				villes.add(ville);
 			}
 			con.close();
-			stmt.close();
-			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return villes;
 	}
@@ -49,12 +66,12 @@ public class VilleDAOImpl implements VilleDAO {
 		Ville ville = null;
 
 		String requete = "SELECT * FROM ville_france WHERE code_postal = " + param;
-		
-
+		Connection con = JDBCConfiguration.getonnexionBDD();
+		Statement stmt = null;
+		ResultSet rs = null;
 		try {
-			Connection con = JDBCConfiguration.getonnexionBDD();
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(requete);
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(requete);
 			while (rs.next()) {
 				ville = new Ville();
 				ville.setCodeCommune(rs.getString(1));
@@ -66,68 +83,133 @@ public class VilleDAOImpl implements VilleDAO {
 				ville.setLongitude(rs.getString(7));
 				villes.add(ville);
 			}
-			rs.close();
-			stmt.close();
-			con.close();
-			
 			return villes;
-			
 		} catch (SQLException e) {
 			System.out.println("Une erreur s'est produite.");
 			return null;
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
 
 	}
 
 	public void setVille(Ville ville) {
-
+		Connection con = null;
+		Statement stmt = null;
 		try {
-			Connection con = JDBCConfiguration.getonnexionBDD();
-			Statement stmt = con.createStatement();
+			con = JDBCConfiguration.getonnexionBDD();
+			stmt = con.createStatement();
 			stmt.executeUpdate(
 					"Insert into ville_france(Code_commune_INSEE,Nom_commune,Libelle_acheminement,Ligne_5,Latitude,Code_postal,Longitude)"
 							+ " values(" + ville.getCodeCommune() + ",'" + ville.getNomCommune() + "','"
 							+ ville.getLibelleAcheminement() + "','" + ville.getLigne() + "'," + ville.getLatitude() + ","
 							+ ville.getCodePostal() + "," + ville.getLongitude() + ")");
-			con.close();
-			stmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
 
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public void mettreAJour(Ville ville) {
-
+		Connection con = null;
+		Statement stmt = null;
 		try {
-			Connection con = JDBCConfiguration.getonnexionBDD();
-			Statement stmt = con.createStatement();
-			String sql = "UPDATE ville_france SET Nom_commune='" + ville.getNomCommune() + "', Code_postal='"
+			con = JDBCConfiguration.getonnexionBDD();
+			stmt = con.createStatement();
+			stmt.executeUpdate("UPDATE ville_france SET Nom_commune='" + ville.getNomCommune() + "', Code_postal='"
 					+ ville.getCodePostal() + "', Libelle_acheminement='" + ville.getLibelleAcheminement() + "', Ligne_5 = '"
 					+ ville.getLigne() + "', Latitude='" + ville.getLatitude() + "', Longitude='" + ville.getLongitude()
-					+ "'  WHERE Code_commune_INSEE='" + ville.getCodeCommune() + "'";
-			System.out.println(sql);
-			stmt.executeUpdate(sql);
-			con.close();
-			stmt.close();
+					+ "'  WHERE Code_commune_INSEE='" + ville.getCodeCommune() + "'");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
 	@Override
 	public void supprimerLigne(String code_commune_INSEE) {
-
+		Connection con = null;
+		Statement stmt = null;
 		try {
-			Connection con = JDBCConfiguration.getonnexionBDD();
-			Statement stmt = con.createStatement();
+			con = JDBCConfiguration.getonnexionBDD();
+			stmt = con.createStatement();
 			stmt.executeUpdate("DELETE FROM ville_france WHERE Code_commune_INSEE = '" + code_commune_INSEE + "'");
-			con.close();
-			stmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
